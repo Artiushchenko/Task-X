c88ac1523ac237c4ca4e4e496f72bd7494dba7ac
Kyrylo Artiushchenko <palmo.artiushchenko.k@gmail.com>
2026-05-18 14:56:47 +0200

    Project initialization

---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+export function Sidebar() {
+	return (
+		<nav className='w-40 bg-teal-400'>
+			<ul>
+				<li>MENU ITEM</li>
+				<li>MENU ITEM</li>
+				<li>MENU ITEM</li>
+				<li>MENU ITEM</li>
+			</ul>
+		</nav>
+	)
+}
